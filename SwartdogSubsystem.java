7aaaea32b8f78d6a012d2879fd8ae100b7eebff2
Collin McIntyre <crm11396@gmail.com>
2022-01-17 15:36:54 -0600

    remove gamemode from subsystem

--- SwartdogSubsystem.java
+++ SwartdogSubsystem.java
@@ -5,14 +5,6 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public abstract class SwartdogSubsystem implements Subsystem
 {
-    public enum GameMode
-    {
-        Disabled,
-        Autonomous,
-        Teleop,
-        Test
-    }
-
     public static boolean REGISTER_SUBSYSTEMS = true;
 
     public SwartdogSubsystem()
@@ -22,6 +14,4 @@ public abstract class SwartdogSubsystem implements Subsystem
             CommandScheduler.getInstance().registerSubsystem(this);
         }
     }
-
-    public abstract void setGameMode(GameMode mode);
 }
